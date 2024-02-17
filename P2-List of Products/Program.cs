af89e52bf8296f7f8a8b1a9fd16ae05dc450948f
Dimityr <dzambazov1986@gmail.com>
2024-02-17 09:19:45 +0200

    Add project files.

---
+++ P2-List of Products/Program.cs
@@ -0,0 +1,18 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+List<string> products = new List<string>();
+string[] productsArray = new string[n];
+
+for (int i = 0; i < n; i++)
+{
+    string product = Console.ReadLine();
+
+    products.Add(product);
+    productsArray[i] = product;
+}
+products.Sort();
+for (int i = 0; i < productsArray.Length; i++)
+{
+
+    Console.WriteLine($"{i + 1}.{products[i]}");
+}
